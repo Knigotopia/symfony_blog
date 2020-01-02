0d8e57a01794d4bf8ce2a7b0f3f9a80a8891c973
Orehov Alexander <sorok@ukr.net>
2020-01-02 18:01:37 +0200

    symfony blog

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     */
+    public function index()
+    {
+        return $this->render('article/index.html.twig', [
+            'controller_name' => 'ArticleController',
+        ]);
+    }
+
+    /**
+     * @Route("/news/", name="article_show")
+     */
+
+    public function show(EntityManagerInterface $em) {
+
+        $repository = $em->getRepository(Article::class);
+        /** @var Article $article */
+        $article = $repository->findAll();
+        if (!$article) {
+            throw $this->createNotFoundException(sprintf('No article for title "%s"', $title));
+        }
+
+        return $this->render('article/show.html.twig', [
+            'title' => $title,
+            'content' => $content,
+        ]);
+    }
+
+}
+
